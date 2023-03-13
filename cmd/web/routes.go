00ac6a56494bb99ef016cd31637306aa9a2246e0
Abu Raihan <raihan2bd.official@gmail.com>
2023-03-13 18:16:50 +0600

    Serve static files from the front-end

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -11,5 +11,8 @@ func (app *application) routes() http.Handler {
 	mux.Get("/virtual-terminal", app.VirtualTerminal)
 	mux.Post("/payment-succeeded", app.PaymentSucceeded)
 
+	fileServer := http.FileServer(http.Dir("./static"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
+
 	return mux
 }
