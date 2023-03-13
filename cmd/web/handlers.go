3ea3f05c1617f82fb7e625178c12259fee99119e
Abu Raihan <raihan2bd.official@gmail.com>
2023-03-13 17:15:15 +0600

    Add environment variables to the project

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,7 +5,12 @@ import (
 )
 
 func (app *application) VirtualTerminal(w http.ResponseWriter, r *http.Request) {
-	if err := app.renderTemplate(w, r, "terminal", nil); err != nil {
+	stringMap := make(map[string]string)
+	stringMap["publishable_key"] = app.config.stripe.key
+
+	if err := app.renderTemplate(w, r, "terminal", &templateData{
+		StringMap: stringMap,
+	}); err != nil {
 		app.errorLog.Println(err)
 	}
 }
