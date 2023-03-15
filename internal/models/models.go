072836fc0461c641b86221873c3477bae5d4b833
Abu Raihan <raihan2bd.official@gmail.com>
2023-03-15 16:31:08 +0600

    Add necessary types to handle the database tables fields

--- internal/models/models.go
+++ internal/models/models.go
@@ -30,8 +30,60 @@ type Widget struct {
 	Description    string    `json:"description"`
 	InventoryLevel int       `json:"inventory_level"`
 	Price          int       `json:"price"`
-	CreatedAt      time.Time `json:"created_at"`
-	UpdatedAt      time.Time `json:"updated_at"`
+	Image          string    `json:"image"`
+	CreatedAt      time.Time `json:"_"`
+	UpdatedAt      time.Time `json:"_"`
+}
+
+// Order is the type for all orders
+type Order struct {
+	ID            int       `json:"id"`
+	WidgetID      int       `json:"widget_id"`
+	TransactionID int       `json:"transaction_id"`
+	StatusID      int       `json:"status_id"`
+	Quantity      int       `json:"quantity"`
+	Amount        int       `json:"amount"`
+	CreatedAt     time.Time `json:"_"`
+	UpdatedAt     time.Time `json:"_"`
+}
+
+// Status is the type for all statuses
+type Status struct {
+	ID        int       `json:"id"`
+	Name      int       `json:"name"`
+	CreatedAt time.Time `json:"_"`
+	UpdatedAt time.Time `json:"_"`
+}
+
+// TransactionStatus is the type for all Transaction Statuses
+type TransactionStatus struct {
+	ID        int       `json:"id"`
+	Name      int       `json:"name"`
+	CreatedAt time.Time `json:"_"`
+	UpdatedAt time.Time `json:"_"`
+}
+
+// Transaction is the type for all Transactions
+type Transaction struct {
+	ID                  int       `json:"id"`
+	Amount              int       `json:"amount"`
+	Currency            string    `json:"currency"`
+	LastFour            string    `json:"last_four"`
+	BankReturnCode      string    `json:"bank_return_code"`
+	TransactionStatusID int       `json:"transaction_status_id"`
+	CreatedAt           time.Time `json:"_"`
+	UpdatedAt           time.Time `json:"_"`
+}
+
+// User is the type for all users
+type User struct {
+	ID        int       `json:"id"`
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	Email     string    `json:"email"`
+	Password  string    `json:"password"`
+	CreatedAt time.Time `json:"_"`
+	UpdatedAt time.Time `json:"_"`
 }
 
 // GetWidget will get widget by id.
