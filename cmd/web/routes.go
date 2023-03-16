0bb4284f6c2a2604d1d1dba9346c6294301b9e63
Abu Raihan <raihan2bd.official@gmail.com>
2023-03-16 19:38:37 +0600

    Refactor virtual terminal page

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -11,11 +11,14 @@ func (app *application) routes() http.Handler {
 	mux.Use(SessionLoad)
 
 	mux.Get("/", app.Home)
+
 	mux.Get("/virtual-terminal", app.VirtualTerminal)
-	mux.Post("/payment-succeeded", app.PaymentSucceeded)
-	mux.Get("/receipt", app.Receipt)
+	mux.Post("/virtual-terminal-payment-succeeded", app.VirtualTerminalPaymentSucceeded)
+	mux.Get("/virtual-terminal-receipt", app.VirtualTerminalReceipt)
 
 	mux.Get("/widget/{id}", app.ChargeOnce)
+	mux.Post("/payment-succeeded", app.PaymentSucceeded)
+	mux.Get("/receipt", app.Receipt)
 
 	fileServer := http.FileServer(http.Dir("./static"))
 	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
