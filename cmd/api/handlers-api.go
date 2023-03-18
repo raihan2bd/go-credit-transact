22ac79bde3ae946dfce6f55c3774ca4ce4536ae7
Abu Raihan <raihan2bd.official@gmail.com>
2023-03-18 09:48:41 +0600

    Add login route inside backend API

--- cmd/api/handlers-api.go
+++ cmd/api/handlers-api.go
@@ -241,3 +241,25 @@ func (app *application) SaveOrder(order models.Order) (int, error) {
 	}
 	return id, nil
 }
+
+func (app *application) CreateAuthToken(w http.ResponseWriter, r *http.Request) {
+	var userInput struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	err := app.readJSON(w, r, &userInput)
+	if err != nil {
+		app.badRequest(w, r, err)
+		return
+	}
+
+	var payload struct {
+		Error   bool   `json:"error"`
+		Message string `json:"message"`
+	}
+	payload.Error = false
+	payload.Message = "Success!"
+
+	_ = app.writeJSON(w, http.StatusOK, payload)
+}
--- cmd/api/routes-api.go
+++ cmd/api/routes-api.go
@@ -24,6 +24,7 @@ func (app *application) routes() http.Handler {
 	mux.Get("/api/widget/{id}", app.GetWidgetByID)
 
 	mux.Post("/api/create-customer-and-subscribe-to-plan", app.CreateCustomerAndSubscribeToPlan)
+	mux.Post("/api/authenticate", app.CreateAuthToken)
 
 	return mux
 }
