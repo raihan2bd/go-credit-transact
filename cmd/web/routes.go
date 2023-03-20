eb7bc485d9cde69052f8c7c936ffb695107d73ab
Abu Raihan <raihan2bd.official@gmail.com>
2023-03-20 21:38:57 +0600

    Add logout functionality to the front-end

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -28,6 +28,8 @@ func (app *application) routes() http.Handler {
 	mux.Get("/receipt/bronze", app.BronzePlanReceipt)
 
 	mux.Get("/login", app.LoginPage)
+	mux.Post("/login", app.PostLoginPage)
+	mux.Get("/logout", app.Logout)
 
 	fileServer := http.FileServer(http.Dir("./static"))
 	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
