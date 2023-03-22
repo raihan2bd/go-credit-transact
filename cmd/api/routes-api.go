8ecf3da549c2d8700c6990f17ccb90a9b17b8739
Abu Raihan <raihan2bd.official@gmail.com>
2023-03-22 18:21:04 +0600

    Display all product sales with customer info

--- cmd/api/routes-api.go
+++ cmd/api/routes-api.go
@@ -33,6 +33,7 @@ func (app *application) routes() http.Handler {
 		mux.Use(app.Auth)
 
 		mux.Post("/virtual-terminal-succeeded", app.VirtualTerminalPaymentSucceeded)
+		mux.Post("/all-sales", app.AllSales)
 
 	})
 
