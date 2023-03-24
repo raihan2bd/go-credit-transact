a2caea7846620a33496b37ff0230191a800063cc
Abu Raihan <raihan2bd.official@gmail.com>
2023-03-24 18:04:57 +0600

    Add invoice routes inside the microservice

---
+++ cmd/micro/invoice/invoice-routers.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+)
+
+func (app *application) routes() http.Handler {
+	mux := chi.NewRouter()
+
+	mux.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	mux.Get("/invoice/create-and-send", app.CreateAndSendInvoice)
+
+	return mux
+}
